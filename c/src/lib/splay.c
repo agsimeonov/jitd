1c6c9aeaac8a069b24e232874930833f9fab8942
Alexander Simeonov <agsimeon@buffalo.edu>
2015-09-21 01:24:27 -0400

    Fully implemented splaying - all splaying steps should now be used.

--- c/src/lib/splay.c
+++ c/src/lib/splay.c
@@ -28,6 +28,70 @@ struct cog *zag(struct cog *root, struct cog *node) {
 }
 
 /**
+ * Zig-Zig step of splaying.
+ *
+ * @param root - current root of the tree
+ * @param node - node to be moved to the root
+ * @return the new root of the rearranged tree
+ */
+struct cog *zigzig(struct cog *root, struct cog *node) {
+  struct cog *parent = root->data.btree.lhs;
+  root->data.btree.lhs = parent->data.btree.rhs;
+  parent->data.btree.lhs = node->data.btree.rhs;
+  parent->data.btree.rhs = root;
+  node->data.btree.rhs = parent;
+  return node;
+}
+
+/**
+ * Zag-Zag step of splaying.
+ *
+ * @param root - current root of the tree
+ * @param node - node to be moved to the root
+ * @return the new root of the rearranged tree
+ */
+struct cog *zagzag(struct cog *root, struct cog *node) {
+  struct cog *parent = root->data.btree.rhs;
+  root->data.btree.rhs = parent->data.btree.lhs;
+  parent->data.btree.rhs = node->data.btree.lhs;
+  parent->data.btree.lhs = root;
+  node->data.btree.lhs = parent;
+  return node;
+}
+
+/**
+ * Zig-Zag step of splaying.
+ *
+ * @param root - current root of the tree
+ * @param node - node to be moved to the root
+ * @return the new root of the rearranged tree
+ */
+struct cog *zigzag(struct cog *root, struct cog *node) {
+  struct cog *parent = root->data.btree.lhs;
+  root->data.btree.lhs = node->data.btree.rhs;
+  parent->data.btree.rhs = node->data.btree.lhs;
+  node->data.btree.lhs = parent;
+  node->data.btree.rhs = root;
+  return node;
+}
+
+/**
+ * Zag-Zig step of splaying.
+ *
+ * @param root - current root of the tree
+ * @param node - node to be moved to the root
+ * @return the new root of the rearranged tree
+ */
+struct cog *zagzig(struct cog *root, struct cog *node) {
+  struct cog *parent = root->data.btree.rhs;
+  root->data.btree.rhs = node->data.btree.lhs;
+  parent->data.btree.lhs = node->data.btree.rhs;
+  node->data.btree.rhs = parent;
+  node->data.btree.lhs = root;
+  return node;
+}
+
+/**
  * The splay operation moves a given node to the root.
  *
  * @param root - current root of the tree
@@ -37,16 +101,23 @@ struct cog *zag(struct cog *root, struct cog *node) {
 struct cog *splay(struct cog *root, struct cog *node) {
   if (node->data.btree.sep <= root->data.btree.sep) {
     if (root->data.btree.lhs == node) {
-      zig(root, node);
+      return zig(root, node);
+    } else if (root->data.btree.lhs->data.btree.lhs == node) {
+      return zigzig(root, node);
+    } else if (root->data.btree.lhs->data.btree.rhs == node) {
+      return zigzag(root, node);
     } else {
-      zig(root, splay(root->data.btree.lhs, node));
+      return zig(root, splay(root->data.btree.lhs, node));
     }
   } else {
     if (root->data.btree.rhs == node) {
-      zag(root, node);
+      return zag(root, node);
+    } else if (root->data.btree.rhs->data.btree.rhs == node) {
+      return zagzag(root, node);
+    } else if (root->data.btree.rhs->data.btree.lhs == node) {
+      return zagzig(root, node);
     } else {
-      zag(root, splay(root->data.btree.rhs, node));
+      return zag(root, splay(root->data.btree.rhs, node));
     }
   }
-  return node;
 }
